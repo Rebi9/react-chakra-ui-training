cc7b5cf5f66ab3e784c3db4782ea16e2a5ede418
Rebi9 <40726530+Rebi9@users.noreply.github.com>
2021-08-14 10:53:51 +0900

    Implemet user edit (Admin user only)

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -10,6 +10,7 @@ import { memo, useCallback, useEffect, VFC } from "react";
 
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { useSelectUser } from "../../hooks/useSelectUser";
+import { useLoginUser } from "../../hooks/useLoginUser";
 import { UserCard } from "../organisms/user/UserCard";
 import { UserDetailModal } from "../organisms/user/UserDetailModal";
 
@@ -17,6 +18,7 @@ export const UserManagement: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
+  const { loginUser } = useLoginUser();
 
   useEffect(() => getUsers(), []);
 
@@ -48,7 +50,12 @@ export const UserManagement: VFC = memo(() => {
           ))}
         </Wrap>
       )}
-      <UserDetailModal user={selectedUser} isOpen={isOpen} onClose={onClose} />
+      <UserDetailModal
+        user={selectedUser}
+        isOpen={isOpen}
+        isAdmin={loginUser?.isAdmin}
+        onClose={onClose}
+      />
     </>
   );
 });
